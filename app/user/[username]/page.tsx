65226b9cfe86e494e196c51fa88e172030568258
secretindex <caio.smelo04@gmail.com>
2024-09-14 23:44:14 -0300

    change username page styles and content

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -2,11 +2,24 @@ import Image from "next/image";
 
 const UserPage = () => {
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col border-[#cecece20] border-x-[1px] w-2/4 h-full mx-auto items-center pt-6">
       <div className="flex flex-col gap-3">
-        <Image src={""} alt="profile image"></Image>
-        <div>
-          <p className="text-sm font-bold">Username</p>
+        <Image
+          src={
+            "https://i.pinimg.com/736x/b9/a3/1c/b9a31c7869815dfddd573a501351f60d.jpg"
+          }
+          className="rounded-full m-auto"
+          width={150}
+          height={150}
+          alt="profile image"
+        ></Image>
+        <div className="text-center w-full">
+          <p className="text-sm font-bold">Gabriel do PvP</p>
+          <div>
+            <span className="text-xs text-[#dedede]">
+              Tenho 10 anos e sou galã de novela
+            </span>
+          </div>
         </div>
       </div>
     </section>
