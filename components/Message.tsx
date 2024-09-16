e90f0f92d250d7c1aad42c99afc33b0756b43069
secretindex <caio.smelo04@gmail.com>
2024-09-15 21:14:49 -0300

    change rounded image to img default

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import RoundedImage from "./small-components/RoundedImage";
 import Link from "next/link";
 
 interface MessageProps {
@@ -10,16 +9,22 @@ interface MessageProps {
 
 const Message: FC<MessageProps> = ({ message, username, profileImage }) => {
   return (
-    <div className="flex flex-row gap-2 w-full">
-      <div className="h-full flex justify-start gap-4">
+    <div className="flex items-center rounded-md flex-row gap-2 w-full cursor-pointer transition-all ease-in-out p-2 hover:bg-[#cecece10]">
+      <div className="h-full">
         <Link href={`/user/${message.length * username.length}`}>
-          <RoundedImage imageUrl={profileImage} alt="user-image" />
+          <img
+            src={profileImage}
+            alt="oi"
+            className="block w-[2.5rem] h-[2.5rem] rounded-full"
+          />
         </Link>
       </div>
       <div className="w-full h-2/3">
         <span>
           <Link href={`/user/${message.length * username.length}`}>
-            <p className="font-bold transition-all ease-in-out hover:underline">{username}</p>
+            <p className="font-bold text-sm transition-all ease-in-out hover:underline">
+              {username}
+            </p>
           </Link>
         </span>
         <p>{message}</p>
